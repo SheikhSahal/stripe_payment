311ad831e957739b228ffd0948aa1b40ec2e2bca
Muhammad Sahal Qasim <36259752+SheikhSahal@users.noreply.github.com>
2021-01-23 19:29:19 +0500

    add_secrt_key_of_stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -7,28 +12,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-source-stripe`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        objects: ["Price"],
+        secretKey: process.env.STRIPE_SECRET_KEY,
+        downloadFiles: false,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
