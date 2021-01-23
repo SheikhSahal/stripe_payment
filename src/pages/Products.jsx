28845ae7c74f6dbe7e31437c2e5045c2d29c9347
Muhammad Sahal Qasim <36259752+SheikhSahal@users.noreply.github.com>
2021-01-23 20:26:18 +0500

    fetch_data_strip_add_strip_payment_gateway

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
-import {graphql, useStaticQuery} from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import { loadStripe } from '@stripe/stripe-js';
 
 
 export default function Home() {
-  
-    const redirectToCheckout = async event => {
+
+    const redirectToCheckout = async (event, pId) => {
         event.preventDefault()
         const stripe = await loadStripe("pk_test_51ICPbvIoW5V7ZqlfNTRdR1YAhQjycy4hOCyWI3rpiUbsmXB3p1qpFkNX4yhTE6C5t3UsLup2XSYNYBI1mrQAE9Td00GvRbVDNE")
         const { error } = await stripe.redirectToCheckout({
-          mode: "subscription",
-          lineItems: [{ price: "price_1ICRUNIoW5V7ZqlfqltqaokE", quantity: 1 }],
-          successUrl: `http://localhost:8000/payment-success/`,
-          cancelUrl: `http://localhost:8000/payment-error/`,
+            mode: "subscription",
+            lineItems: [{ price: pId, quantity: 1 }],
+            successUrl: `http://localhost:8000/payment-success/`,
+            cancelUrl: `http://localhost:8000/payment-error/`,
         })
         if (error) {
-          console.warn("Error:", error)
-                    
+            console.warn("Error:", error)
+
         }
-      }
+    }
 
     const data = useStaticQuery(graphql`
     query MyQuery {
         allStripePrice {
           edges {
-            next {
+            node {
               product {
                 id
                 description
@@ -32,15 +32,28 @@ export default function Home() {
                 updated
                 images
               }
+              id
             }
           }
         }
       }
+      
     `)
 
     console.log(data);
 
-    return <>
-     <button onClick={redirectToCheckout}>Buy Laptops</button>
-     </>
+    return <div>
+        <h1>Products</h1>
+        {
+            data.allStripePrice.edges.map(({ node }) => {
+                return <div key={node.id}>
+                    <p>{node.product.name}</p>
+                    <p>{node.product.description}</p>
+                    <img src={node.product.images[0]} />
+                    <button onClick={(e)=> redirectToCheckout(e,node.id)}>Buy {node.product.name}</button>
+                </div>
+            })
+        }
+ 
+    </div>
 }
